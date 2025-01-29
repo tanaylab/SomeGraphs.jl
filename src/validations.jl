5a49f9f0f47f2b10e76dce17c121191d629f5024
Oren Ben-Kiki <oren@ben-kiki.org>
2025-01-29 11:27:28 +0200

    Add validations.

--- src/validations.jl
+++ src/validations.jl
@@ -14,6 +14,31 @@ module Validations
 
 export ValidationContext
 export location
+export validate
+export validate_is_above
+export validate_is_at_least
+export validate_is_at_most
+export validate_is_below
+export validate_is_color
+export validate_matrix_entries
+export validate_matrix_size
+export validate_vector_entries
+export validate_vector_length
+
+using Colors
+
+"""
+    Maybe{T} = Union{T, Nothing}
+
+The type to use when maybe there is a value, maybe there isn't. This is exactly as if writing the explicit `Union`
+with `Nothing` but is shorter and more readable. This is extremely common.
+
+!!! note
+
+    This is replicated from `DataAxesFormats` to avoid making it a dependency. We do not export it but use it
+    extensively in the type signatures.
+"""
+Maybe = Union{T, Nothing} where {T}
 
 """
 A context (path of field names and/or indices) leading to a validated value. A string indicates access of a data member,
@@ -57,6 +82,174 @@ function location(context::ValidationContext)::AbstractString
     end
 
     return join(text)
+end
+
+"""
+A common type for objects that support validation, that is, that one can invoke [`validate`](@ref) on.
+"""
+abstract type Validated end
+
+"""
+    validate(context::ValidationContext, value::Validated)::Nothing end
+    validate(context::ValidationContext, value::Validated, extra::Any)::Nothing end
+
+Validate the `value` which was accessed via the `context`, possibly using some `extra` informative. Will throw
+`ArgumentError` if the value isn't valid.
+"""
+function validate(context::ValidationContext, value::Validated)::Nothing end  # NOLINT
+function validate(context::ValidationContext, value::Validated, extra::Any)::Nothing end  # NOLINT
+
+"""
+    validate_is_at_least(context::ValidationContext, value::Maybe{Real}, minimum::Real)::Nothing
+
+Validate that a `value` is at least some `minimum` (if it is specified).
+"""
+function validate_is_at_least(context::ValidationContext, value::Maybe{Real}, minimum::Real)::Nothing
+    if value !== nothing && value < minimum
+        throw(ArgumentError("too low $(location(context)): $(value)\nis not at least: $(minimum)"))
+    end
+    return nothing
+end
+
+"""
+    validate_is_above(context::ValidationContext, value::Maybe{Real}, minimum::Real)::Nothing
+
+Validate that a `value` is above some `minimum` (if it is specified).
+"""
+function validate_is_above(context::ValidationContext, value::Maybe{Real}, minimum::Real)::Nothing
+    if value !== nothing && value <= minimum
+        throw(ArgumentError("too low $(location(context)): $(value)\nis not above: $(minimum)"))
+    end
+    return nothing
+end
+
+"""
+    validate_is_at_most(context::ValidationContext, value::Maybe{Real}, maximum::Real)::Nothing
+
+Validate that a `value` is at most some `maximum` (if it is specified).
+"""
+function validate_is_at_most(context::ValidationContext, value::Maybe{Real}, maximum::Real)::Nothing
+    if value !== nothing && value > maximum
+        throw(ArgumentError("too high $(location(context)): $(value)\nis not at most: $(maximum)"))
+    end
+    return nothing
+end
+
+"""
+    validate_is_below(context::ValidationContext, value::Maybe{Real}, maximum::Real)::Nothing
+
+Validate that a `value` is below some `maximum` (if it is specified).
+"""
+function validate_is_below(context::ValidationContext, value::Maybe{Real}, maximum::Real)::Nothing
+    if value !== nothing && value >= maximum
+        throw(ArgumentError("too high $(location(context)): $(value)\nis not below: $(maximum)"))
+    end
+    return nothing
+end
+
+"""
+    validate_is_color(context::ValidationContext, color::Maybe{AbstractString})::Nothing
+
+Validate that a `color` is a valid color name (if it is specified).
+"""
+function validate_is_color(context::ValidationContext, color::Maybe{AbstractString})::Nothing
+    if color !== nothing
+        try
+            parse(Colorant, color)  # NOJET
+        catch
+            throw(ArgumentError("invalid $(location(context)): $(color)"))
+        end
+    end
+    return nothing
+end
+
+"""
+    validate_vector_length(context::ValidationContext, vector::Maybe{AbstractVector}, expected_length::Integer)::Nothing
+
+Validate that a `vector` has the `expected_length` (if it is specified).
+"""
+function validate_vector_length(
+    context::ValidationContext,
+    vector::Maybe{AbstractVector},
+    expected_length::Integer,
+)::Nothing
+    if vector !== nothing && length(vector) != expected_length
+        throw(
+            ArgumentError(
+                "invalid length of $(location(context)): $(length(vector))\n" *
+                "is different from expected length: $(expected_length)",
+            ),
+        )
+    end
+    return nothing
+end
+
+"""
+    validate_vector_entries(validation::Function, context::ValidationContext, vector::Maybe{AbstractVector})::Nothing
+
+Validate all the entries of a `vector` using the `validation` function. It is given the entry's index, and its value.
+The context is updated to include the index for the duration of the function.
+"""
+function validate_vector_entries(
+    validation::Function,
+    context::ValidationContext,
+    vector::Maybe{AbstractVector},
+)::Nothing
+    if vector !== nothing
+        for (index, entry) in enumerate(vector)
+            push!(context, index)
+            validation(index, entry)
+            pop!(context)  # NOJET
+        end
+    end
+    return nothing
+end
+
+"""
+    validate_matrix_size(context::ValidationContext, matrix::Maybe{AbstractMatrix}, expected_size::Tuple{Integer, Integer})::Nothing
+
+Validate that a `matrix` has the `expected_size` (if it is specified).
+"""
+function validate_matrix_size(
+    context::ValidationContext,
+    matrix::Maybe{AbstractMatrix},
+    expected_size::Tuple{Integer, Integer},
+)::Nothing
+    if matrix !== nothing && size(matrix) != expected_size
+        throw(
+            ArgumentError(
+                "invalid size of $(location(context)): $(size(matrix))\n" *
+                "is different from expected size: $(expected_size)",
+            ),
+        )
+    end
+    return nothing
+end
+
+"""
+    validate_matrix_entries(validation::Function, context::ValidationContext, matrix::Maybe{AbstractMatrix})::Nothing
+
+Validate all the entries of a `matrix` using the `validation` function. It is given the entry's row and column indices,
+and its value. The context is updated to include the indices for the duration of the function.
+"""
+function validate_matrix_entries(
+    validation::Function,
+    context::ValidationContext,
+    matrix::Maybe{AbstractMatrix},
+)::Nothing
+    if matrix !== nothing
+        n_rows, n_columns = size(matrix)
+        for row_index in 1:n_rows
+            push!(context, row_index)
+            for column_index in n_columns
+                push!(context, column_index)
+                validation(row_index, column_index, matrix[row_index, column_index])
+                pop!(context)
+            end
+            pop!(context)
+        end
+    end
+    return nothing
 end
 
 end
--- test/validations.jl
+++ test/validations.jl
@@ -16,4 +16,107 @@ nested_test("validations") do
             @test location(ValidationContext(["foo", "bar", 1, 2, "baz"])) == "foo.bar[1, 2].baz"
         end
     end
+
+    nested_test("bounds") do
+        context = ValidationContext(["foo"])
+
+        nested_test("at_least") do
+            validate_is_at_least(context, 0, 0)
+            @test_throws dedent("""
+                too low foo: -1
+                is not at least: 0
+            """) validate_is_at_least(context, -1, 0)
+        end
+
+        nested_test("above") do
+            validate_is_above(context, 1, 0)
+            @test_throws dedent("""
+                too low foo: 0
+                is not above: 0
+            """) validate_is_above(context, 0, 0)
+        end
+
+        nested_test("at_most") do
+            validate_is_at_most(context, 1, 1)
+            @test_throws dedent("""
+                too high foo: 2
+                is not at most: 1
+            """) validate_is_at_most(context, 2, 1)
+        end
+
+        nested_test("below") do
+            validate_is_below(context, 0, 1)
+            @test_throws dedent("""
+                too high foo: 1
+                is not below: 1
+            """) validate_is_below(context, 1, 1)
+        end
+    end
+
+    nested_test("color") do
+        context = ValidationContext(["foo_color"])
+
+        validate_is_color(context, "red")
+        validate_is_color(context, "#FF0000")
+        validate_is_color(context, "#FF0000FF")
+        @test_throws "invalid foo_color: Oobleck" validate_is_color(context, "Oobleck")
+    end
+
+    nested_test("vector") do
+        context = ValidationContext(["foo"])
+
+        vector = [0, 1]
+
+        nested_test("length") do
+            validate_vector_length(context, vector, 2)
+            @test_throws dedent("""
+                invalid length of foo: 2
+                is different from expected length: 1
+            """) validate_vector_length(context, vector, 1)
+        end
+
+        nested_test("entries") do
+            validate_vector_entries(context, vector) do index, value
+                return validate_is_at_most(context, value, 1)
+            end
+
+            @test_throws dedent("""
+                too high foo[2]: 1
+                is not below: 1
+            """) begin
+                validate_vector_entries(context, vector) do index, value
+                    return validate_is_below(context, value, 1)
+                end
+            end
+        end
+    end
+
+    nested_test("matrix") do
+        context = ValidationContext(["foo"])
+
+        matrix = [5 4 3; 2 1 0]
+
+        nested_test("size") do
+            validate_matrix_size(context, matrix, (2, 3))
+            @test_throws dedent("""
+                invalid size of foo: (2, 3)
+                is different from expected size: (3, 2)
+            """) validate_matrix_size(context, matrix, (3, 2))
+        end
+
+        nested_test("entries") do
+            validate_matrix_entries(context, matrix) do row, column, value
+                return validate_is_at_least(context, value, 0)
+            end
+
+            @test_throws dedent("""
+                too low foo[2, 3]: 0
+                is not above: 0
+            """) begin
+                validate_matrix_entries(context, matrix) do row, column, value
+                    return validate_is_above(context, value, 0)
+                end
+            end
+        end
+    end
 end
